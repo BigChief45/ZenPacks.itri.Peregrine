ddede8798fb3806fab6cd4588f2d5383b4411f46
Andres Alvarez <jalvarez@itri.org.tw>
2016-04-11 11:44:30 +0800

    Make the plugin return the relationship map

--- ZenPacks/itri/Peregrine/modeler/plugins/itri/peregrine/Services.py
+++ ZenPacks/itri/Peregrine/modeler/plugins/itri/peregrine/Services.py
@@ -3,6 +3,7 @@
 # stdlib Imports
 import json
 import urllib
+import base64
 
 # Twisted Imports
 from twisted.internet.defer import inlineCallbacks, returnValue
@@ -15,9 +16,14 @@ from Products.ZenUtils.Utils import prepId
 
 class Services(PythonPlugin):
     """Peregrine services modeler plugin."""
+    
+    relname = 'peregrineServices'
+    modname = 'ZenPacks.itri.Peregrine.PeregrineService'
 
     requiredProperties = (
         'zPeregrinePort',
+        'zPeregrineUser',
+        'zPeregrinePassword',
     )
 
     deviceProperties = PythonPlugin.deviceProperties + requiredProperties
@@ -33,7 +39,19 @@ class Services(PythonPlugin):
         # Get the API port and validate it if it's set
         peregrine_port = getattr(device, 'zPeregrinePort', None)
         if not peregrine_port:
-            log.error("%s: %s not set. Please set port to connect to the Peregrine API")
+            log.error("%s: %s not set. Please set port to connect to the Peregrine API", device.id, 'zPeregrinePort')
+            returnValue(None)
+        
+        # Get the API user and validate it if it's set
+        peregrine_user = getattr(device, 'zPeregrineUser', None)
+        if not peregrine_user:
+            log.error("%s: %s not set. Please set username to connect to the Peregrine API", device.id, 'zPeregrineUser')
+            returnValue(None)
+            
+        # Get the API password and validate it if it's set
+        peregrine_password = getattr(device, 'zPeregrinePassword', None)
+        if not peregrine_password:
+            log.error("%s: %s not set. Please set password to connect to the Peregrine API", device.id, 'zPeregrinePassword')
             returnValue(None)
         
         # Get the Peregrine API base URL
@@ -42,32 +60,34 @@ class Services(PythonPlugin):
         # Get the Peregrine Services
         results['services'] = []
         try:
-            services = yield getPage(base_url + '/showStatus')
-            services = jsonloads(services)
-            #results['services'] = ...
+            # Build the authentication headers
+            basicAuth = base64.encodestring("%s:%s" % (peregrine_user, peregrine_password))
+            authHeader = "Basic " + basicAuth.strip()
+        
+            services = yield getPage(base_url + '/getAgentStatus',
+                       headers={"content-type": "application/json", "Authorization": authHeader}
+                       )
+            services = json.loads(services)
+            results['services'] = services['services']['agents']
         except Exception, e:
             log.error("%s: %s", device.id, e)
             returnValue(None)
         
-        log.debug("Peregrine Services: %s\n" % str(services))
+        log.info("Peregrine Services: %s\n" % str(services))
         
-        returnValue(results)
+        rm = self.relMap()
+        
+        # Process services
+        for service in results['services']:
+            rm.append(self.objectMap({
+                'id' : prepId(service['ip']),
+                'title' : service['ip'],
+                'status' : service['status'],
+                }))
+                                                                                                            
+        
+        returnValue(rm)
     
     def process(self, device, results, log):
         """ Process results. Return iterable of datamaps or None."""
-        
-        log.info("%s: Processing data...", device.id)
-        
-        # Process services
-        services = []
-        for service in results['services']:
-            services.append(ObjectMap(
-                modname='ZenPacks.itri.Peregrine.PeregrineService',
-                data=dict(
-                    id=prepId(''),
-                    title= '',
-                    agent= '',
-                    status= '',
-                )))
-                
-        return None
+        return results
